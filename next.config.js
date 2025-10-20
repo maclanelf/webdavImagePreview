26c05e5e0878692aad4534419eb520e976b5ccd6
王泷炜 <gw00354006@mind-ea.cn>
2025-10-20 17:33:47 +0800

    创建docker配置文件

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  // 启用 standalone 输出模式，用于 Docker 部署
+  output: 'standalone',
   images: {
     domains: [],
     remotePatterns: [
